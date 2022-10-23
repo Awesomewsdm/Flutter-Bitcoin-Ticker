8d02568cf6d39430a06947925ae0231b30805142
Awesomewsdm <88857387+Awesomewsdm@users.noreply.github.com>
2022-10-24 00:11:19 +0100

    starting project

---
+++ lib/newfile.dart
@@ -0,0 +1,32 @@
+// // List<int> winningNumbers = [12, 6, 34, 22, 41, 9];
+
+// // void main() {
+// //   List<int> ticket1 = [45, 2, 9, 18, 12, 33];
+// //   List<int> ticket2 = [41, 17, 26, 32, 7, 35];
+
+// //   checkNumbers(ticket1);
+// // }
+
+// // void checkNumbers(List<int> myNumbers) {
+// //   int matches = 0;
+// //   for (int number in myNumbers) {
+// //     for (int winNum in winningNumbers) {
+// //       if (winNum == number) {
+// //         matches++;
+// //       }
+// //     }
+// //   }
+// //   print('You have $matches matches');
+// // }
+
+// import 'package:musicolet/coin_data.dart';
+
+// void main(List<String> args) {
+//   getDropDownItem();
+// }
+
+// void getDropDownItem() {
+//   for (String currency in currenciesList) {
+//     print(currency);
+//   }
+// }
